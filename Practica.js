1f0280068841111c16bc45790a3301aad84df624
NehuenIniguez <nehuen.iniguez@gmail.com>
2024-04-17 12:12:22 -0300

    Mayor-Menor

--- Practica.js
+++ Practica.js
@@ -1,6 +1,24 @@
+//ejercicio  1
 const num=5
 const nume=7
 let suma= num + nume
 
 
 console.log (suma)
+
+//ejercicio 2
+const var1=3
+const var2=7
+const var3=1
+
+if (var1 > var2 && var1 > var3)
+    console.log("el mayor es " , var1)
+        else if (var2>var3 && var2>var1)
+            console.log ("el mayor es ", var2)
+                else console.log ("el mayor es ", var3)
+
+if  (var1<var2 && var1<var3)
+    console.log("el menor es", var1)
+        else if (var2<var1 && var2<var3)
+            console.log("el menor es", var2)
+                else console.log ("el menor es", var3)
